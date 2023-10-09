9dec7f99ff4f1adb35ddf7d9f2e933ec58f2d2d2
Scoges <scott.ogrins@gmail.com>
2023-10-09 12:37:08 -0300

    changed routes to switch in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
@@ -36,11 +37,11 @@ function App() {
       <Router>
         <>
           <Navbar />
-          <Routes>
+          <Switch>
             <Route path='/' element={<SearchBooks />} />
             <Route path='/saved' element={<SavedBooks />} />
             <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>} />
-          </Routes>
+          </Switch>
         </>
       </Router>
     </ApolloProvider>
