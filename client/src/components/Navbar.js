e1e02dfa141c80c02dda2e5675721f86eb9a3078
Scoges <scott.ogrins@gmail.com>
2023-10-19 16:03:52 -0300

    heroku bug

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,13 +14,13 @@ const AppNavbar = () => {
     <>
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
+          <Navbar.Brand as={Link} to='*'>
             Google Books Search
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
             <Nav className='ml-auto d-flex'>
-              <Nav.Link as={Link} to='/'>
+              <Nav.Link as={Link} to='*'>
                 Search For Books
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
-const express = require('express');
 
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
